aa9d8eab6e157d23fd19987f1a308f8b6d791768
64356 <64356@DESKTOP-19BM6I4>
2022-07-28 18:52:29 +0300

    Добавьте файлы проекта.

---
+++ Homework5.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5._1
+{
+
+    internal class Program
+    {
+        //Задание 1
+        /// <summary>
+        /// Метод, который из строки выводит масив слов.
+        /// </summary>
+        /// <param name="Text">Строковая переменная, которую нужно переобразить в массив</param>
+        /// <returns></returns>
+        static string[] Textinput(string Text)
+        {
+
+            string[] words = Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+           
+            return words;
+        }
+        /// <summary>
+        /// Метод который разделяет строку на слова и выводит на экран.
+        /// </summary>
+        /// <param name="words">Массив, который нужно разделить</param>
+        static void Print(string[] words)
+        {
+            foreach (string c in words)
+            { 
+                Console.WriteLine(words);
+            }
+           
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите предложение");
+            string Text = Console.ReadLine();
+            string[] words = Textinput(Text);
+            Print(words);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}   
---
+++ Задание 2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_2
+{
+    class Program
+    {
+        /// <summary>
+        /// Метод разделяющий слова в предложении
+        /// </summary>
+        /// <param name="Text">Строковая переменная, которую нужно разделить в массив</param>
+        /// <returns></returns>
+        static string SplitWords(string Text)
+        {
+            string[] arrayWord = Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var e in arrayWord)
+            {
+                Console.WriteLine(e);
+            }
+            string newText = ReverseWords(arrayWord);
+            return newText;
+        }
+        /// <summary>
+        /// Метод меняющий слова местами
+        /// </summary>
+        /// <param name="Text">Массив, слова которого необходимо поменять местами </param>
+        /// <returns></returns>
+        static string ReverseWords(string[] Text)
+        {
+            Array.Reverse(Text);
+            string newString = string.Join(" ", Text);
+            return newString;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string inputPhrase = Console.ReadLine();
+
+            inputPhrase = SplitWords(inputPhrase);
+            Console.WriteLine(inputPhrase);
+            
+            Console.ReadKey();
+        }
+    }
+}      
