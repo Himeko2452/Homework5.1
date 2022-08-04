409f61def6b2859316e4e1fa6a976bd5e1108d84
Himeko2452 <97700677+Himeko2452@users.noreply.github.com>
2022-08-04 14:39:34 +0300

    Update Program.cs

--- Задание 2/Program.cs
+++ Задание 2/Program.cs
@@ -6,42 +6,44 @@ using System.Threading.Tasks;
 
 namespace Задание_2
 {
-    class Program
-    {
-        /// <summary>
+    /// <summary>
         /// Метод разделяющий слова в предложении
         /// </summary>
         /// <param name="Text">Строковая переменная, которую нужно разделить в массив</param>
         /// <returns></returns>
-        static string SplitWords(string Text)
+
+
+        static string[] SplitWords(string Text)
         {
             string[] arrayWord = Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             foreach (var e in arrayWord)
             {
                 Console.WriteLine(e);
-            }
-            string newText = ReverseWords(arrayWord);
-            return newText;
+            }              
+            return arrayWord;
         }
         /// <summary>
         /// Метод меняющий слова местами
         /// </summary>
         /// <param name="Text">Массив, слова которого необходимо поменять местами </param>
         /// <returns></returns>
-        static string ReverseWords(string[] Text)
+
+        
+
+        static string ReverseWords(string Text)
         {
-            Array.Reverse(Text);
-            string newString = string.Join(" ", Text);
+            string[] arrayWord = SplitWords(Text);
+            Array.Reverse(arrayWord);
+            string newString = string.Join(" ", arrayWord);
+
             return newString;
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Введите предложение: ");
             string inputPhrase = Console.ReadLine();
-
-            inputPhrase = SplitWords(inputPhrase);
-            Console.WriteLine(inputPhrase);
-            
+            string arrayWord = ReverseWords(inputPhrase);
+            Console.WriteLine(arrayWord);            
             Console.ReadKey();
         }
     }
